72f3a222d6687da539ee0e93f2cd6ccb8ded215e
PreFKim <pre_f_86@naver.com>
2024-01-30 19:57:17 +0900

    Update linear.py (convert to array operation)

--- src/layers/linear.py
+++ src/layers/linear.py
@@ -1,4 +1,4 @@
-from ..parameter import Param
+from ..array import from_numpy
 from .module import Module
 import numpy as np
 
@@ -8,46 +8,12 @@ class Linear(Module):
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.bias = bias
-
-
-        self.w = [[Param(1) for j in range(self.in_channels) ] for i in range(self.out_channels)] # out_channels, in_channels
-        
-        for i in range(self.out_channels): # He Initialize
-            for j in range(self.in_channels):
-                self.w[i][j].data = np.random.uniform(-np.sqrt(6/in_channels),np.sqrt(6/in_channels))
+        self.w = from_numpy(np.random.uniform(-np.sqrt(6/in_channels),np.sqrt(6/in_channels),size=(in_channels,out_channels)))
 
         if self.bias:
-            self.b = [Param(1) for i in range(out_channels)] # out_channels
-
-            for i in range(self.out_channels): # He Initialize
-                self.b[i].data = np.random.uniform(-np.sqrt(1/in_channels),np.sqrt(1/in_channels))
+            self.b = from_numpy(np.random.uniform(-np.sqrt(1/in_channels),np.sqrt(1/in_channels),size=(out_channels)))
         
         
     def forward(self,x):
-
-        ret = []
-        for i in range(self.out_channels):
-            sum = x[0] * self.w[i][0]
-            for j in range(1,self.in_channels):
-                sum = sum + self.w[i][j] * x[j]
-            ret.append(sum)
-
-        if self.bias:
-            for i in range(self.out_channels):
-                ret[i] = ret[i] + self.b[i]
-        
-        return ret
-    
-    def parameters(self):
-        ret = []
-        for i in range(self.out_channels):
-            for j in range(self.in_channels):
-                ret.append(self.w[i][j])
-
-        if self.bias:
-            for i in range(self.out_channels):
-                ret.append(self.b[i])
-        return ret
-    
-
-
+        if self.bias : return x @ self.w + self.b
+        else : return x @ self.w 
