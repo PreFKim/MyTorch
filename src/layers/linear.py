441a54de3cdf68733e45b1800e7f263a2b36dff1
Kim Tae Hoon <pre_f_86@naver.com>
2024-11-03 16:24:29 +0900

    Change Linear Weight initialize logic (Kaiming Init)
    - Change package relative directory to absolute directory
    
        - ex) ./src/gradients/basic.py, ./src/ops/basic.py
    
    - It was discovered that the existing initialization method causes the weights to be too large when in_channels is less than 6, leading to poor learning performance.
    
        - Reference
        	- https://github.com/pytorch/pytorch/blob/main/torch/nn/modules/linear.py#L118 # Linear Weight Init
    	- https://github.com/pytorch/pytorch/blob/main/torch/nn/modules/conv.py#L178 # Conv Weight Init
    	- https://github.com/pytorch/pytorch/blob/main/torch/nn/init.py#L456 # kaiming Uniform Init
    	- https://pytorch.org/docs/stable/nn.init.html

--- src/layers/linear.py
+++ src/layers/linear.py
@@ -1,5 +1,5 @@
-from .module import Module
-from ..parameter import Param
+from src.layers.module import Module
+from src.parameter import Param
 import numpy as np
 
 class Linear(Module):
@@ -7,15 +7,26 @@ class Linear(Module):
 
         self.in_channels = in_channels
         self.out_channels = out_channels
+        
+        # kaiming uniform init
+        # fan = _calculate_correct_fan(tensor, mode) # in_channels
+        # gain = calculate_gain(nonlinearity, a) # math.sqrt(2.0 / (1 + a**2)) [a=5] -> 0.2773500981126146
+        # std = gain / math.sqrt(fan) # gain / (in_channels) **(1/2)
+        # bound = math.sqrt(3.0) * std # 1.7320508075688772 * std
+        
+        bound = 1.7320508075688772 * 0.2773500981126146/(in_channels)**(1/2)
+
         self.weight = Param(
-            # np.ones((in_channels, out_channels)),
-            np.random.uniform(-np.sqrt(6/in_channels), np.sqrt(6/in_channels), size=(in_channels, out_channels)), 
+            # np.ones((in_channels, out_channels)).astype(np.float32),
+            # np.random.uniform(-np.sqrt(6/in_channels), np.sqrt(6/in_channels), size=(in_channels, out_channels)).astype(np.float32), 
+            # np.random.normal(loc=0, scale=np.sqrt(2/in_channels), size=(in_channels, out_channels)).astype(np.float32),
+            np.random.uniform(-bound, bound, size=(in_channels, out_channels)).astype(np.float32), 
             requires_grad=True
             )
 
         self.bias = Param(
-            # np.ones((out_channels)),
-            np.random.uniform(-np.sqrt(1/in_channels), np.sqrt(1/in_channels), size=(out_channels)),
+            # np.ones((out_channels)).astype(np.float32),
+            np.random.uniform(-np.sqrt(1/in_channels), np.sqrt(1/in_channels), size=(out_channels)).astype(np.float32),
             requires_grad=True
             ) if bias else None
         
