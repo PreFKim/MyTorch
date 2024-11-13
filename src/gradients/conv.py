2b418add7feed6705802d834022e9d38e4261c5a
Kim Tae Hoon <pre_f_86@naver.com>
2024-11-13 21:23:54 +0900

    Update conv.py

--- src/gradients/conv.py
+++ src/gradients/conv.py
@@ -23,9 +23,9 @@ class Convolution(Function):
         node_data = np.pad(node.data, pad_width=pad_width)
 
         if len(weight.shape) == 3: # Conv1d
-            ret = Conv1d_forward(node_data, weight, out_shape, stride)
+            ret = Conv1d_forward(node_data, weight.data, out_shape, stride)
         elif len(weight.shape) == 4: # Conv2d
-            ret = Conv2d_forward(node_data, weight, out_shape, stride)
+            ret = Conv2d_forward(node_data, weight.data, out_shape, stride)
         else:
             raise NotImplementedError(f"Conv{len(weight.shape)-2}d operation is not implemented")
 
@@ -50,9 +50,9 @@ class Convolution(Function):
         node_data = np.pad(node.data, pad_width=pad_width)
 
         if len(weight.shape) == 3: # Conv1d
-            grad_node, grad_weight = Conv1d_backward(node_data, weight, grad, stride, padding, batch_first)
+            grad_node, grad_weight = Conv1d_backward(node_data, weight.data, grad, stride, padding, batch_first)
         elif len(weight.shape) == 4: # Conv2d
-            grad_node, grad_weight = Conv2d_backward(node_data, weight, grad, stride, padding, batch_first)
+            grad_node, grad_weight = Conv2d_backward(node_data, weight.data, grad, stride, padding, batch_first)
         else:
             raise NotImplementedError(f"Conv{len(weight.shape)-2}d operation is not implemented")
 
@@ -71,7 +71,7 @@ def Conv1d_forward(data, weight, out_shape, stride):
     ret = np.zeros(out_shape, dtype=data.dtype)
     for c in range(out_channels): 
         for i in range(left, l-right, stride[0]):
-            ret[..., c, (i-left)//stride[0]] = (data[..., i-left:i+right+1] * weight.data[c]).sum((-1, -2)) # batch, in_channels, kernel_size -> batch, 1, 1
+            ret[..., c, (i-left)//stride[0]] = (data[..., i-left:i+right+1] * weight[c]).sum((-1, -2)) # batch, in_channels, kernel_size -> batch, 1, 1
     return ret
 
 def Conv1d_backward(data, weight, grad, stride, padding, batch_first):
@@ -86,7 +86,7 @@ def Conv1d_backward(data, weight, grad, stride, padding, batch_first):
     grad_weight = np.zeros((data.shape[0], *weight.shape), dtype=data.dtype) if batch_first else np.zeros_like(weight)
 
     for i in range(grad_l):
-        grad_node[..., (i*stride[0]): (i*stride[0])+left+right+1] += (weight.data * grad[..., np.newaxis, i:i+1]).sum(-3)
+        grad_node[..., (i*stride[0]): (i*stride[0])+left+right+1] += (weight * grad[..., np.newaxis, i:i+1]).sum(-3)
         # grad_node -> (batch), in_channels, out_length+2*padding
         # weight -> out_channels, in_channels, kernel_size 
         # grad -> (batch), out_chennels, out_length -> indexing -> (batch), out_channels, 1, 1
@@ -114,7 +114,7 @@ def Conv2d_forward(data, weight, out_shape, stride):
     for c in range(out_channels): 
         for i in range(top, h-bottom, stride[0]):
             for j in range(left, w-right, stride[1]):
-                ret[..., c, (i-top)//stride[0], (j-left)//stride[1]] = (data[..., i-top:i+bottom+1, j-left:j+right+1] * weight.data[c]).sum((-1, -2, -3)) 
+                ret[..., c, (i-top)//stride[0], (j-left)//stride[1]] = (data[..., i-top:i+bottom+1, j-left:j+right+1] * weight[c]).sum((-1, -2, -3)) 
     return ret
 
 def Conv2d_backward(data, weight, grad, stride, padding, batch_first):
@@ -132,7 +132,7 @@ def Conv2d_backward(data, weight, grad, stride, padding, batch_first):
     
     for i in range(grad_h):
         for j in range(grad_w):
-            grad_node[..., (i*stride[0]): (i*stride[0])+top+bottom+1, (j*stride[1]): (j*stride[1])+left+right+1] += (weight.data * grad[..., np.newaxis, i:i+1, j:j+1]).sum(-4)
+            grad_node[..., (i*stride[0]): (i*stride[0])+top+bottom+1, (j*stride[1]): (j*stride[1])+left+right+1] += (weight * grad[..., np.newaxis, i:i+1, j:j+1]).sum(-4)
     
     grad_node = grad_node[..., padding[0]:h-padding[0], padding[1]:w-padding[1]]
 
